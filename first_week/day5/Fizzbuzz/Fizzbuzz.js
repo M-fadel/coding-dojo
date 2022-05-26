43b35ba34a1f2a4ec087e80c8180ba2271eabc7b
M-fadel <m.ohm.md2008@hotmail.com>
2022-05-26 13:16:05 +0300

    Commit

--- first_week/day5/Fizzbuzz/Fizzbuzz.js
+++ first_week/day5/Fizzbuzz/Fizzbuzz.js
@@ -1,13 +1,13 @@
 for (let index = 1; index <= 100; index++) {
-    if(index%3 ==0 && index %5 ==0){
+    if (index % 3 == 0 && index % 5 == 0) {
         console.log("FizzBuzz");
-    }else if(index %3 ==0){
-            console.log("FIZZ");
-    }else if(index %5 == 0 ){
+    } else if (index % 3 == 0) {
+        console.log("FIZZ");
+    } else if (index % 5 == 0) {
         console.log("BUZZ")
-    }else{
+    } else {
         console.log(index)
 
     }
-    
+
 }
