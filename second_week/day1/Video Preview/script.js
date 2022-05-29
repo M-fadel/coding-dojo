cfab07d419ad8276d9125d306557953e5ced3f33
M-fadel <m.ohm.md2008@hotmail.com>
2022-05-29 21:28:33 +0300

    commit

--- second_week/day1/Video Preview/script.js
+++ second_week/day1/Video Preview/script.js
@@ -1,10 +1,10 @@
 console.log("page loaded...");
 
-function playvid(element){
-    // element.play();
-if document.getElementById.
-//  var temp= element.
-//  if (){
-//      element.play();
-//  }
+function over(element){
+    element.play();
+
+}
+function out(element){
+element.pause();
+
 }
