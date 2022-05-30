3730b8e04c163b8e626b3d29db708a7d8cd4000b
M-fadel <m.ohm.md2008@hotmail.com>
2022-05-31 02:00:45 +0300

    Commit

--- second_week/day2/Making Connections/script.js
+++ second_week/day2/Making Connections/script.js
@@ -1,7 +1,22 @@
 console.log("page loaded...");
 
-    function changeName(element){
-element.parentElement.querySelector('h1').innerText ="Mohammed Fadel";
+function changeName(element) {
+    element.parentElement.querySelector('h1').innerText = "Mohammed Fadel";
 
 
-    }
+}
+function accept(e) {
+    var t = []
+    t = document.querySelectorAll(".badge");
+    t[0].innerText--;
+    // t[1].innerText++;
+
+
+}
+
+function decline(e) {
+    t = document.querySelectorAll(".badge");
+    t[0].innerText--;
+    e.parentElement.parentElement.remove();
+
+}
