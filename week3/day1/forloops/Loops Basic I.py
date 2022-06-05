31e3af72f52f34e091f5f57fed25e02a9c80b508
M-fadel <m.ohm.md2008@hotmail.com>
2022-06-05 15:26:46 +0300

    commit

---
+++ week3/day1/forloops/Loops Basic I.py
@@ -0,0 +1,26 @@
+for x in range(151):
+    print(x)    
+
+for y in range(5, 1000,5):
+    print(y)
+
+for x in range(100):
+    if x%10==0: 
+        print("Coding Dojo")
+    elif x%5 ==0:
+        print("Coding")
+
+hug = 0
+for y in range(500000):
+    hug += y 
+print(hug)
+
+for x in range(2018, 0,-4):
+    print(x)
+
+lowNum=2
+highNum=9
+mult=3
+for x in range(lowNum, highNum+1):
+    if x%mult == 0:
+        print(x)
