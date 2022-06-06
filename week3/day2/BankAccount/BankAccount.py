2d484fd5d88d98c35e2a130275d424d63d636e69
M-fadel <m.ohm.md2008@hotmail.com>
2022-06-07 01:31:39 +0300

    commit

--- week3/day2/BankAccount/BankAccount.py
+++ week3/day2/BankAccount/BankAccount.py
@@ -6,25 +6,27 @@ class BankAccount:
     
     def deposit(self, amount):
         self.balance += amount
+        return self
     
     def withdraw(self, amount):
         if amount>self.balance:
             print( "Insufficient funds: Charging a $5 fee" )
             self.balance -=5
+            return self
         else:
             self.balance -= amount
+            return self
 
     def display_account_info(self):
         print("balance: ",self.balance)
+        return self
         
 
     def yield_interest(self):
         if self.balance>0:
             self.balance = self.balance * self.int_rate
+            return self
 
 acc1 = BankAccount(5,1000)
-acc1.deposit(10)
-acc1.withdraw(5)
-acc1.display_account_info()
-acc1.yield_interest()
-acc1.display_account_info()
+acc1.deposit(10).deposit(10).deposit(10).withdraw(1000).yield_interest().display_account_info()
+acc2 = BankAccount(0.1,500).deposit(10).deposit(10).withdraw(100).withdraw(100).withdraw(100).withdraw(100).yield_interest().display_account_info()
