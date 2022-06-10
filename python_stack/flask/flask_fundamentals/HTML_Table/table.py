d7a987dfcfad1781f46514591f6cb0c8270c39bd
M-fadel <m.ohm.md2008@hotmail.com>
2022-06-10 20:44:00 +0300

    commit

--- python_stack/flask/flask_fundamentals/HTML_Table/table.py
+++ python_stack/flask/flask_fundamentals/HTML_Table/table.py
@@ -1,3 +1,7 @@
+from queue import Full
+from flask import Flask, render_template
+app = Flask(__name__)
+
 users = [
     {'first_name' : 'Michael', 'last_name' : 'Choi'},
     {'first_name' : 'John', 'last_name' : 'Supsupin'},
@@ -9,19 +13,28 @@ names = []
 lengthOfNames =0
 for i in users:
         l=list(i.keys())
-print(l[0])
-
+l.append("full_name")
 def iterateDictionary(some_list):
     for dict in some_list:
         names.append (dict.get('first_name'))
         names.append(dict.get('last_name'))
         names.append(dict.get('first_name')+" "+dict.get('last_name'))
-    lengthOfNames= int(len(names)/3)
+    lengthOfNames= int(len(names))
     return lengthOfNames
     
 lengthOfNames= iterateDictionary(users)
 
-print(names)
-print(lengthOfNames)
-# a =users[0]
-# a= users[0]
+@app.route('/')
+def something():
+    
+    return render_template("index.html",hhead = l,table =names,tablehead=len(l),tableR =lengthOfNames)
+
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+print(users)
